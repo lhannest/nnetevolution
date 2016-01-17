6ccfd9ea1dfe6328623e972947b2462b5501ba12
lhannest <lancehannestad@gmail.com>
2016-01-16 23:01:11 -0800

    Gave isInput and isOutput properties to Node in components.py, implemented getParent() function in evolution.py

--- src/components.py
+++ src/components.py
@@ -19,6 +19,14 @@ class Node(object):
 		self.incoming = UniqueList()
 		self.outgoing = UniqueList()
 
+	@property
+	def isInput(self):
+		return len(self.incoming) == 0
+
+	@property
+	def isOutput(self):
+		return len(self.outgoing) == 0
+
 	def __eq__(self, other):
 		if isinstance(other, Node):
 			return self.__innovation == other.__innovation
--- src/evolution.py
+++ src/evolution.py
@@ -4,11 +4,22 @@
 ### -- This should have the effect of somewhat modularizing the neural network
 import random
 
-def getParent(source_node, num_steps=0):
-	# a recurrsive function that counts the number of ancestors of
-	# source_node and then randomly chooses one to return
-	arc = random.choice(source_node.incoming)
+def getParent(source_node):
+	return __getParent(source_node, 0)
+	
+def __getParent(source_node, num_steps):
+	# recursively chooses an ansestor of source_node and returns it
+	if source_node.isInput:
+		return random.uniform(1, num_steps + 1)
 
+	arc = random.choice(source_node.incoming)
+	next_step = num_steps + 1
+	choice = getParent(arc.parent, next_step)
+
+	if choice == num_steps:
+		return source_node
+	else:
+		return choice
 
 def addNode(node_collection):
 	source_node = random.choice(node_collection)
